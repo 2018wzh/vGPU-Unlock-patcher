be336bdf5fafbac3c1858b98f168fe0d2ff94247
Benjamin Doron <benjamin.doron00@gmail.com>
2026-05-07 14:14:03 -0400

    patches: Patch sources for compatibility with newer kernels
    Ported from GRID 20.0.
    
    Signed-off-by: Benjamin Doron <benjamin.doron00@gmail.com>

--- patch.sh
+++ patch.sh
@@ -619,6 +619,7 @@ $TDMABUFEXPORT && {
     applypatch ${TARGET} test-dmabuf-export.patch
 }
 $DO_VGPU && applypatchx ${TARGET} vgpu-kvm-support-v6.18-namespaced-variables.patch
+$DO_VGPU && applypatchx ${TARGET} vgpu-kvm-support-v6.19-pfn_address_space.patch
 $DO_VGPU && applypatchx ${TARGET} vgpu-kvm-fault-path-acquires-mmap_lock.patch
 $DO_VGPU && applypatchx ${TARGET} vgpu-kvm-optional-vgpu-v2.patch
 
